6e88fea8c0a9c97309dc9be90a1d2b35dab4c02c
Ange Arsene <nkenmandenga@gmail.com>
2025-02-04 10:17:27 +0000

    Enhance Collection class with documentation and initialize method for data setup

--- src/collections/Collection.php
+++ src/collections/Collection.php
@@ -11,16 +11,26 @@ namespace WoowaWebhooks\Collections;
  */
 abstract class Collection
 {
+    /**
+     * @var array $data The data stored in the collection.
+     */
     protected static array $data;
 
+    /**
+     * Initialize the collection with the given payload.
+     * 
+     * @param array $payload The data to initialize the collection.
+     */
     protected static function bootstrap(array $payload): void
     {
+        // Set the data for the collection
         self::$data = $payload;
     }
         
     /**
      * Filter the collection data.
      * 
+     * @param array $data The data to filter.
      * @return array The filtered data.
      */
     abstract public static function filter(array $data): array;

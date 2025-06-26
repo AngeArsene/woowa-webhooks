31e5cc8e65968a0129f2052e3a8fe8df31a8b8a0
Ange Arsene <nkenmandenga@gmail.com>
2025-06-26 11:07:20 +0100

    Refactor is_seven_days_before function: streamline date parsing and improve readability by removing unnecessary line breaks.

--- utils/utilities.php
+++ utils/utilities.php
@@ -217,13 +217,9 @@ function flatten_array(array $array): array
  */
 function is_seven_days_before(string $date = ''): bool
 {
-    $input_date = DateTime::createFromFormat(
-        'm/d/Y', str_replace("'", '', $date)
-    );
+    $input_date = DateTime::createFromFormat('m/d/Y', str_replace("'", '', $date));
 
-    if (!$input_date) {
-        return true; // Invalid date format
-    }
+    if (!$input_date) return true; // Invalid date format
 
     $now = new DateTime('now', new DateTimeZone('UTC'));
     $seven_days_ago = $now->modify('-3 days');

885fb281dd02d8c8ad993b130afea6298d285d13
Ange Arsene <nkenmandenga@gmail.com>
2025-06-26 15:30:55 +0100

    Refactor DateUtilitiesTest: add test for jakarta_date function

--- tests/DateUtilitiesTest.php
+++ tests/DateUtilitiesTest.php
@@ -2,7 +2,10 @@
 
 namespace WoowaWebhooks\Tests;
 
+use DateTime;
+use DateTimeZone;
 use PHPUnit\Framework\TestCase;
+use function jakarta_date;
 use function is_seven_days_before;
 
 /**
@@ -26,5 +29,22 @@ final class DateUtilitiesTest extends TestCase
         $this->assertTrue(is_seven_days_before($further_date));
         
         $this->assertFalse(is_seven_days_before(date('m/d/Y')));
+    }
+
+    /**
+     * Tests the jakarta_date function to ensure it returns the correct
+     * date and time in Jakarta for a given time offset.
+     *
+     * @return void
+     */
+    public function testJakartaDate(): void
+    {
+        $time_offset = 'now';
+
+        $date = new DateTime($time_offset, new DateTimeZone("Asia/Jakarta"));
+        $date = $date->format('Y-m-d H:i');
+
+        $this->assertEquals($date, jakarta_date($time_offset));
+        $this->assertNotEquals($date, jakarta_date('tomorrow'));
     }
 }
